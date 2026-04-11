3b9eb78b1de67781d62333a76feaeb5495f2b2f6
marketingiantrading-maker <marketing.iantrading@gmail.com>
2026-04-11 16:38:59 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+export const SUPABASE_CONFIG = {
+  url: 'https://oaauobugpbcixnbmlakc.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hYXVvYnVncGJjaXhuYm1sYWtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MzYyNTksImV4cCI6MjA5MTQxMjI1OX0.EpOUpmwRx_LAUqpGQcVMyJFB5NqX_HM7aMZ2gWW9oAA'
+}
